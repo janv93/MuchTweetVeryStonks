8d9b5d7e2204fe5e991a84d7ac3f95e27c562bdd
Jan <jazevo@web.de>
2021-05-20 23:30:33 +0200

    feat (twitter) finish twitter functionality

---
+++ index.js
@@ -0,0 +1,69 @@
+require('dotenv').config();
+const axios = require('axios');
+
+const interval = 1000000;  // seconds
+
+checkTwitter().then(res => {
+  const data = res.data;
+  const newTweet = getNewTweet(data.data);
+
+  if (newTweet) {
+    processTweet(newTweet.text);
+  }
+}).catch(err => {
+  console.log(err.response.data);
+});
+
+
+function checkTwitter () {
+  const options = {
+    headers: {
+      Authorization: 'Bearer ' + process.env.twitter_bearer_token
+    }
+  };
+
+  return axios.get('https://api.twitter.com/2/users/44196397/tweets?exclude=retweets,replies&tweet.fields=created_at', options);
+}
+
+function getNewTweet(tweets) {
+  const newTweets = tweets.filter(tweet => {
+    const date = new Date(tweet.created_at);
+    const now = new Date();
+
+    const timeDiff = Math.abs(date-now) / 1000;
+    
+    if (timeDiff <= interval) {
+      return true;
+    }
+  });
+
+  return newTweets.length > 0 ? newTweets[0] : null;
+}
+
+function processTweet(tweet) {
+  const message = tweet.toLowerCase();
+  const dogeSignals = ['doge'];
+  const btcSignals = ['btc', 'bitcoin'];
+
+  const containsDoge = dogeSignals.find(signal => {
+    return tweet.includes(signal);
+  });
+
+  const containsBtc = btcSignals.find(signal => {
+    return tweet.includes(signal);
+  });
+
+  console.log(message);
+
+  if (containsDoge) {
+    processDoge();
+  }
+
+  if (containsBtc) {
+    processBtc();
+  }
+}
+
+function processDoge() {
+  console.log('oh he did not..')
+}
